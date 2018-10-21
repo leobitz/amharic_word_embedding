a8211f0b223fb8f4d4635629e8f2dc5b23c2ff2a
LeoBitz <amanyamy@gmail.com>
2018-10-22 00:15:27 +0300

    RNN model

--- trainer_demo.py
+++ trainer_demo.py
@@ -1,17 +1,40 @@
 from tf_trainer import *
 from word2vec import *
-from gensim_wrapper import *
+from data_handle import *
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-batch_size = 1000
+batch_size = 120
 embedding_size = 128
 skip_window = 5
-num_skips = 2
-trainer = Trainer(train_name="full_200", 
-                batch_size=batch_size,
-                  skip_window=skip_window,
-                  max_words=-1)
-vocab_size = trainer.vocab_size
 
-trainer.train(embedding_size, epoches=40)
+words = read_file()
+vocab, word2int, int2word = build_vocab(words)
+word2freq = get_frequency(words, word2int, int2word)
+unigrams = [word2freq[int2word[i]] for i in range(len(word2int))]
+
+vocab_size = len(vocab)
+steps_per_batch = len(words) // batch_size
+
+int_words = words_to_ints(word2int, words)
+print("Final train data: {0}".format(len(words)))
+gen = generate_batch_embed(int_words, batch_size, skip_window)
+
+graph = tf.Graph()
+with graph.as_default():
+    w2v_model = Word2Vec(vocab_size=vocab_size,
+                         embed_size=embedding_size,
+                         num_sampled=5,
+                         batch_size=batch_size,
+                         unigrams=unigrams)
+
+
+trainer = Trainer(train_name="full_200")
+
+
+trainer.train(graph=graph,
+              model=w2v_model,
+              gen=gen,
+              steps_per_batch=steps_per_batch,
+              embed_size=embedding_size,
+              epoches=40)
--- word2vec.py
+++ word2vec.py
@@ -5,13 +5,12 @@ from tensorflow.python.ops import candidate_sampling_ops
 
 class Word2Vec:
 
-    def __init__(self, vocab_size, wordfreq, embed_size=128, batch_size=128, num_sampled=64, unigrams=None):
+    def __init__(self, vocab_size, embed_size=128, batch_size=128, num_sampled=64, unigrams=None):
         self.vocab_size = vocab_size
         self.embedding_size = embed_size
         self.batch_size = batch_size
         self.num_sampled = num_sampled
         self.unigrams = unigrams
-        self.word_freq = wordfreq
         self.build()
         # self._forward()
 
@@ -30,7 +29,7 @@ class Word2Vec:
                 tf.int32, shape=[self.batch_size, 1])
 
     def _create_embedding(self):
-        with tf.device('/GPU:0'):
+        with tf.device('/CPU:0'):
             # Look up embeddings for inputs.
             with tf.name_scope('embeddings_layer'):
                 init_width = 0.5 / self.embedding_size
@@ -71,8 +70,9 @@ class Word2Vec:
 
     def _create_optimizer(self):
         with tf.name_scope('optimizer'):
-            self.optimizer = tf.train.GradientDescentOptimizer(
-                1.0).minimize(self.loss)
+            self.optimizer = tf.train.MomentumOptimizer(learning_rate=.1,
+                                                        momentum=0.9,
+                                                        use_nesterov=True).minimize(self.loss)
 
     def _create_final_embedding(self):
         norms = tf.sqrt(tf.reduce_sum(
@@ -100,99 +100,3 @@ class Word2Vec:
             [self.loss, self.optimizer],
             feed_dict=feed_dict)
         return loss_val
-
-    # def _forward(self):
-    #     """Build the graph for the forward pass."""
-    #     opts = self
-    #     examples = self.train_inputs
-    #     labels = self.train_labels
-    #     word_freq = self.word_freq
-    #     # Declare all variables we need.
-    #     # Embedding: [vocab_size, emb_dim]
-    #     init_width = 0.5 / self.embedding_size
-    #     emb = tf.Variable(
-    #         tf.random_uniform(
-    #             [opts.vocab_size, opts.embedding_size], -init_width, init_width),
-    #         name="emb")
-    #     self.embeddings = emb
-
-    #     # Softmax weight: [vocab_size, emb_dim]. Transposed.
-    #     sm_w_t = tf.Variable(
-    #         tf.zeros([opts.vocab_size, opts.embedding_size]),
-    #         name="sm_w_t")
-
-    #     # Softmax bias: [emb_dim].
-    #     sm_b = tf.Variable(tf.zeros([opts.vocab_size]), name="sm_b")
-
-    #     # Global step: scalar, i.e., shape [].
-    #     self.global_step = tf.Variable(0, name="global_step")
-
-    #     # Nodes to compute the nce loss w/ candidate sampling.
-    #     labels_matrix = tf.cast(labels, dtype=tf.int64)
-
-    #     # Negative sampling.
-    #     sampled_ids, _, _ = (tf.nn.fixed_unigram_candidate_sampler(
-    #         true_classes=labels_matrix,
-    #         num_true=1,
-    #         num_sampled=opts.num_sampled,
-    #         unique=True,
-    #         range_max=opts.vocab_size,
-    #         distortion=0.75,
-    #         unigrams=list(word_freq.values()))
-
-    #     # Embeddings for examples: [batch_size, emb_dim]
-    #     example_emb=tf.nn.embedding_lookup(emb, examples)
-
-    #     # Weights for labels: [batch_size, emb_dim]
-    #     true_w=tf.nn.embedding_lookup(sm_w_t, labels)
-    #     # Biases for labels: [batch_size, 1]
-    #     true_b=tf.nn.embedding_lookup(sm_b, labels)
-
-    #     # Weights for sampled ids: [num_sampled, emb_dim]
-    #     sampled_w=tf.nn.embedding_lookup(sm_w_t, sampled_ids)
-    #     # Biases for sampled ids: [num_sampled, 1]
-    #     sampled_b=tf.nn.embedding_lookup(sm_b, sampled_ids)
-
-    #     # True logits: [batch_size, 1]
-    #     true_logits=tf.reduce_sum(tf.mul(example_emb, true_w), 1) + true_b
-
-    #     # Sampled logits: [batch_size, num_sampled]
-    #     # We replicate sampled noise lables for all examples in the batch
-    #     # using the matmul.
-    #     sampled_b_vec=tf.reshape(sampled_b, [opts.num_sampled])
-    #     sampled_logits=tf.matmul(example_emb,
-    #                                sampled_w,
-    #                                transpose_b=True) + sampled_b_vec
-    #     return true_logits, sampled_logits
-
-    # def _nce_loss(self, true_logits, sampled_logits):
-    #     """Build the graph for the NCE loss."""
-
-    #     # cross-entropy(logits, labels)
-    #     opts=self
-    #     true_xent=tf.nn.sigmoid_cross_entropy_with_logits(
-    #         true_logits, tf.ones_like(true_logits))
-    #     sampled_xent=tf.nn.sigmoid_cross_entropy_with_logits(
-    #         sampled_logits, tf.zeros_like(sampled_logits))
-
-    #     # NCE-loss is the sum of the true and noise (sampled words)
-    #     # contributions, averaged over the batch.
-    #     nce_loss_tensor=(tf.reduce_sum(true_xent) +
-    #                        tf.reduce_sum(sampled_xent)) / opts.batch_size
-    #     return nce_loss_tensor
-
-    # def _optimize(self, loss):
-    #     """Build the graph to optimize the loss function."""
-
-    #     # Optimizer nodes.
-    #     # Linear learning rate decay.
-    #     opts=self
-    #     words_to_train=float(opts.words_per_epoch * opts.epochs_to_train)
-    #     lr=opts.learning_rate * tf.maximum(
-    #         0.0001, 1.0 - tf.cast(self._words, tf.float32) / words_to_train)
-    #     self._lr=lr
-    #     optimizer=tf.train.GradientDescentOptimizer(lr)
-    #     train=optimizer.minimize(loss,
-    #                                global_step=self.global_step,
-    #                                gate_gradients=optimizer.GATE_NONE)
-    #     self._train=train
